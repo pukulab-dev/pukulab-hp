fedb89007ae615ed99758918d8530cbf08dad01d
pukulab-dev <pukulab.dev.0301@gmail.com>
2026-07-05 14:28:41 +0900

    修正

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router-dom/server";
+import { StaticRouter } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
 
